a5123f3bf87e977974688849ec56e4dddeabbdb1
tareksek <tareknegoudi@gmail.com>
2025-04-06 17:47:55 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const telegramConfig = {
+  apiToken: 'YOUR_TELEGRAM_BOT_TOKEN',
+  channelId: '@YourChannelName', // معرف القناة التي سيستمع لها البوت
+  serverEndpoint: 'https://your-server.com/upload-video'
+};
