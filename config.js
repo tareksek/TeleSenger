6d54201b1eda6852144125d8a44ceda24f0259ae
tareksek <tareknegoudi@gmail.com>
2025-04-06 19:16:04 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const telegramConfig = {
-  apiToken: '7418986006:AAG8NOkbpqLmtePil_MsIJkWFgUAzbBUkmg',
-  channelId: '@technia22', // معرف القناة التي سيستمع لها البوت
-  serverEndpoint: 'https://telesenger.onrender.com/upload-video'
-};
